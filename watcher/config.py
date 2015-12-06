2bb9ddd15a5f28c4f06417e70307c89f8d65afc6
Gilang <gilank.gepe@gmail.com>
2015-12-07 06:56:39 +0700

    Update config.py

--- watcher/config.py
+++ watcher/config.py
@@ -1,7 +1,7 @@
-CUSTOMER_KEY = 'OTjg3W9kcRfoUJaPHq3Q68aMK'
-CUSTOMER_SECRET = 'cgUSZAAWvixQWA29pO1jgmhB7pgxcomdXDiW8kzgZI6bpkGDH4'
-ACCESS_TOKEN_KEY = '100960321-uylZKezeR8ppFsMDstInxJ2b7ybSCbhBrnXy0lSR'
-ACCESS_TOKEN_SECRET = 'Y66Don3d8tXTkraj2xni5mVGnInf6gEzcJfGsOCNjdIkK'
+CUSTOMER_KEY = 'xxx'
+CUSTOMER_SECRET = 'xxx'
+ACCESS_TOKEN_KEY = 'xxx'
+ACCESS_TOKEN_SECRET = 'xxx'
 
 # Twitter ID to follow
 # Get twitter id from http://gettwitterid.com/
